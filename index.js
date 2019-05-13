4e7fb5e86a0dcfddf8650fe9d7564cf76ba88c4a
David zhang <d.ydzdavid@yahoo.com>
2019-05-13 02:58:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file
+function findMatching (drivers, name) {
+  let newdriver =  drivers.filter( function (driver) { driver.toLowerCase() === name.toLowerCase();} );
+  return newdriver;
+}
